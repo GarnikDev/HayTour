5375ab0d1ee15f8413089440fb6d4c7c2fba7099
Garnik Mikayelyan <garnik.mikayelian@gmail.com>
2025-12-07 23:14:43 +0100

    1st Commit Test

---
+++ demo/demo/src/main/java/com/sevitours/demo/SevitoursApplication.java
@@ -0,0 +1,11 @@
+package com.sevitours.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevitoursApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SevitoursApplication.class, args);
+	}
+}
---
+++ demo/demo/src/test/java/com/sevitours/demo/SevitoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevitours.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevitoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
